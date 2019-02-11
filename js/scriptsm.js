b40706f0ca2c4ab3a0e88357c4e5f3332cf17440
LuisJCastano <luisjc99@yahoo.com>
2019-02-11 18:23:48 +0100

    Carro

---
+++ js/scriptsm.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $( window ).on( "load", function(){
+        $.ajax({
+            url: "js/pelis.json",
+            type: "GET",
+            dataType: "json",
+            success: function(datos){
+                for(i=0;i<3;i++){
+                    $("#n"+i ).append(datos[i].nombre);
+                    $("#d"+i ).append(datos[i].desc);
+                }
+            }
+        });
+    });
+
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+      });
+
+});
+   
+    
