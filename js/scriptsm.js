e41af9af58b78c084f64a11f67e002a12ce68ee1
smchirila <aluy2893781g@ieselcaminas.org>
2019-02-12 12:54:14 +0100

    commit 4

--- js/scriptsm.js
+++ js/scriptsm.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+/*
     $( window ).on( "load", function(){
         $.ajax({
             url: "js/pelis.json",
@@ -13,11 +13,43 @@ $(document).ready(function(){
             }
         });
     });
+*/
 
     $("#myBtn").click(function(){
         $("#myModal").modal();
       });
 
+
+      $("#btn-r").click(function(){
+        
+        var err;
+
+        if($("#nombre").val() == ""){
+            err = "El campo Nombre no puede estar vacío.</b>!";
+            $("#err").html("El campo Nombre no puede estar vacío.</b>!");
+            //alert("El campo Nombre no puede estar vacío.");
+            $("#nombre").focus();       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+            return false;
+        }
+        
+        if($("#email").val() == ""){
+            err = "El campo email no puede estar vacío.</b>!";
+            $("#err").html("El campo email no puede estar vacío.</b>!");
+            //alert("El campo email no puede estar vacío.");
+            $("#email").focus();
+            return false;
+        }
+
+        if($("#pass").val() == ""){
+            //err = "El campo email no puede estar vacío.</b>!";
+            //alert("La contraseña no puede estar vacía.");
+            $("#err").html("La contraseña no puede estar vacía</b>!");
+            $("#pass").focus();
+            return false;
+        }
+
+      });
+    
 });
    
    
