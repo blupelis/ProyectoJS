9214e5b03f8df362fa082a92c6e28d6b7d54ab7c
martmota98 <martinimartinz98@gmail.com>
2019-02-14 09:43:53 +0100

    2

--- js/sriptmr.js
+++ js/sriptmr.js
@@ -1,6 +1,37 @@
 $( function() {
-    $( "#button" ).on( "click", function() {
-      $( ".newClass" ).switchClass( "newClass", "anotherNewClass", 1000 );
-      $( ".anotherNewClass" ).switchClass( "anotherNewClass", "newClass", 1000 );
-    });
-  } );
+  $( "#button" ).on( "click", function() {
+    $( ".newClass" ).switchClass( "newClass", "anotherNewClass", 1000 );
+    $( ".anotherNewClass" ).switchClass( "anotherNewClass", "newClass", 1000 );
+  });
+} );
+
+$(document).ready(function(){
+  
+  // Donde queremos cambiar el tamaño de la fuente
+  var donde = $('.mitexto');
+  var sizeFuenteOriginal = donde.css('font-size');
+  
+  // Resetear Font Size
+  $(".resetearFont").click(function(){
+  donde.css('font-size', sizeFuenteOriginal);
+  });
+ 
+  // Aumentar Font Size
+  $(".aumentarFont").click(function(){s
+  	var sizeFuenteActual = donde.css('font-size');
+ 	var sizeFuenteActualNum = parseFloat(sizeFuenteActual, 10);
+    var sizeFuenteNuevo = sizeFuenteActualNum*1.2;
+	donde.css('font-size', sizeFuenteNuevo);
+	return false;
+  });
+ 
+  // Disminuir Font Size
+  $(".disminuirFont").click(function(){
+  	var sizeFuenteActual = donde.css('font-size');
+ 	var sizeFuenteActualNum = parseFloat(sizeFuenteActual, 10);
+    var sizeFuenteNuevo = sizeFuenteActualNum*0.8;
+	donde.css('font-size', sizeFuenteNuevo);
+	return false;
+  });
+  
+});
